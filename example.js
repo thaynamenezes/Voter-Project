2820ca18c559d468ec22f61d356796342d84da53
Patricio de la Guardia <padlgt01@gmail.com>
2018-08-23 09:31:37 -0400

    Add relative pathing to headless browser setup

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
+const path = require('path');
 const puppeteer = require('puppeteer');
 
 (async() => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`${__dirname}/index.html`);
+	await page.goto(`file:${path.join(__dirname, 'index.html')}`);
   await page.screenshot({path: `${__dirname}/example.png`});
-  
   await browser.close();
 })();
